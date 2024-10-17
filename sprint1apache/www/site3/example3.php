ddbea01be87e1764d17f01faadfd17ee40090a13
Julieta <jlancellottii23@fpcoruna.afundacion.org>
2024-10-17 12:16:37 +0200

    Jubilación

---
+++ sprint1apache/www/site3/example3.php
@@ -0,0 +1,19 @@
+<html>
+	<body>
+		<h1>Jubilación</h1>
+		<?php
+		  function edad_en_10_años($edad) {
+		    return $edad + 10;
+		}
+
+		
+		$edad= $_GET["edad"];
+
+		if (edad_en_10_años($edad) > 65) {
+		  echo "En 10  años tendrás edad de jubilación";
+		} else {
+		  echo "¡Disfruta de tu tiempo!";
+		}
+	      ?>
+	</body>
+</html>
