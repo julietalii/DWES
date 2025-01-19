c22a545b583a9cc8423af85c0b111216109aa07a
Julieta <jlancellottii23@fpcoruna.afundacion.org>
2025-01-19 19:57:47 +0100

    Semana 1: coonfigurar settings.py

--- Eval2sprint1/RestAPI/Tarea1/models.py
+++ Eval2sprint1/RestAPI/Tarea1/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class Usuarios(AbstractUser):
+    rol=models.CharField(max_length=20)
+    biografia=models.CharField(max_length=100)
+    def __str__(self):
+        return self.username
+
+class Eventos(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_hora = models.DateTimeField()
+    capacidad = models.IntegerField()
+    url = models.URLField()
+    def __str__(self):
+        return self.titulo
+
+class Comentarios(models.Model):
+    texto = models.TextField()
+    id_evento = models.ForeignKey(Eventos, on_delete=models.CASCADE)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.texto
+
+class Reservas(models.Model):
+    informacion = models.TextField()
+    id_evento = models.ForeignKey(Eventos, on_delete=models.CASCADE)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    entradas = models.IntegerField()
+    estado = models.CharField(max_length=20)
+    def __str__(self):
+        return self.informacion
 # Create your models here.
+
