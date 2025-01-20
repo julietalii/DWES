cb4fcf0e9200b593c5a48a949ff449f68571f917
Julieta <jlancellottii23@fpcoruna.afundacion.org>
2025-01-20 20:37:24 +0100

    Semana 1: admin.py

--- Eval2sprint1/RestAPI/Tarea1/admin.py
+++ Eval2sprint1/RestAPI/Tarea1/admin.py
@@ -1,18 +1,16 @@
 from django.contrib import admin
 from .models import Usuarios, Eventos, Reservas, Comentarios
 
-admin.site.register(Usuarios)
-admin.site.register(Eventos)
-admin.site.register(Reservas)
-admin.site.register(Comentarios)
-
 
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'fecha_hora', 'capacidad')
-    search_fields = ('titulo', 'descripcion')
-    list_filter = ('fecha_hora')
+    search_fields = ('titulo',)
+    list_filter = ('fecha_hora',)
 
-
+admin.site.register(Usuarios)
+admin.site.register(Eventos, EventoAdmin)
+admin.site.register(Reservas)
+admin.site.register(Comentarios)
 
 
 # Register your models here.
