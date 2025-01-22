d777fa9b1aba0752496ff86ce26e7613fd1c27df
Julieta <jlancellottii23@fpcoruna.afundacion.org>
2025-01-22 13:43:05 +0100

    Semana 2: Primer ejercicio

--- Eval2sprint1/RestAPI/Tarea1/views.py
+++ Eval2sprint1/RestAPI/Tarea1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from Tarea1.models import Eventos
 
+def listar_eventos(request):
+    eventos = Eventos.objects.all()
+    data = [{"titulo": p.titulo} for p in eventos]
+    return JsonResponse(data, safe=False)
 # Create your views here.
